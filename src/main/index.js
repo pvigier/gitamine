60f6eab0e23ccbee90cb6cd62b4d8f7f02f1c253
Pierre Vigier <pierre.vigier@ymail.com>
2018-12-12 23:38:09 +0100

    Bootstrap the project

---
+++ src/main/index.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  console.log('Application ready!');
+
+  mainWindow = new BrowserWindow({
+    show: false,
+    width:  800,
+    height: 600,
+    backgroundColor: '#1e1e1e'
+  });
+
+  mainWindow.webContents.on('dom-ready', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/../../index.html`);
+  
+});
+

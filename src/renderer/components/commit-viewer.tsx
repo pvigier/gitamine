c756b7ce41b2ef3f3e3d298d0b310824c89d44c4
Pierre Vigier <pierre.vigier@ymail.com>
2018-12-20 23:09:25 +0100

    Add PatchItem

--- src/renderer/components/commit-viewer.tsx
+++ src/renderer/components/commit-viewer.tsx
@@ -1,12 +1,50 @@
 import * as React from 'react';
 import * as Git from 'nodegit';
+import { PatchItem } from './patch-item';
 
 export interface CommitViewerProps { commit: Git.Commit | null; }
+export interface CommitViewerState { patches: Git.ConvenientPatch[]; }
 
-export class CommitViewer extends React.Component<CommitViewerProps, {}> {
+export class CommitViewer extends React.Component<CommitViewerProps, CommitViewerState> {
+  commit: Git.Commit | null;
+  constructor(props: CommitViewerProps) {
+    super(props);
+    this.commit = this.props.commit;
+    this.state = {
+      patches: []
+    }
+  }
+
+  updatePatches(commit: Git.Commit) {
+    commit.getDiff()
+      .then((diffs) => diffs.length > 0 ? diffs[0].patches() : [])
+      .then((patches) => {
+        this.setState({
+          patches: patches
+        })
+      });
+  }
+
   render() {
-    const commit = this.props.commit;
+    let patchesDirty = false;
+    if (this.commit !== this.props.commit) {
+      this.commit = this.props.commit; 
+      patchesDirty = true;
+    }
+    const commit = this.commit;
     if (commit) {
+      // Patches
+      const patches = this.state.patches;
+      let patchesItems: JSX.Element[] = [];
+      if (patchesDirty) {
+        this.updatePatches(commit);
+      } else {
+        patchesItems = patches.map((patch) => {
+          const path = patch.newFile().path();
+          return <PatchItem onPatchSelect={() => {}} patch={patch} key={path} />
+        })
+      }
+
       const authoredDate = new Date(commit.author().when().time() * 1000);
       return (
         <div className='commit-viewer'>
@@ -15,6 +53,9 @@ export class CommitViewer extends React.Component<CommitViewerProps, {}> {
           <p>Authored {authoredDate.toString()} by {commit.author().name()}</p>
           <p>Last modified {commit.date().toString()}</p>
           <p>Parents: {commit.parents().map((sha) => sha.tostrS().substr(0, 8)).toString()}</p>
+          <ul>
+            {patchesItems}
+          </ul>
         </div>
       );
     } else {
---
+++ src/renderer/components/patch-item.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as Git from 'nodegit';
+
+export interface PatchItemProps { 
+  patch: Git.ConvenientPatch;
+  onPatchSelect: (patch: Git.ConvenientPatch) => void;
+}
+
+export class PatchItem extends React.Component<PatchItemProps, {}> {
+  constructor(props: PatchItemProps) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e: React.MouseEvent<HTMLLIElement>) {
+    this.props.onPatchSelect(this.props.patch);
+  }
+
+  render() {
+    const path = this.props.patch.newFile().path();
+    return <li onClick={this.handleClick}>{path}</li>;
+  }
+}

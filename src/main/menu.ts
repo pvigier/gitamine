46609fea07d9b25faa2ada47487bf2bf53c7f337
Pierre Vigier <pierre.vigier@ymail.com>
2019-01-06 23:15:54 +0100

    Add code to open modal windows

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,10 +1,34 @@
-import { Menu, dialog } from 'electron';
+import { BrowserWindow, Menu, dialog } from 'electron';
+
+function createModalWindow(parent: BrowserWindow, path: string) {
+  let win: BrowserWindow | null = new BrowserWindow({
+    show: false,
+    width: 400,
+    height: 320,
+    parent: parent,
+    modal: true
+  });
+
+  win.loadURL(path);
+
+  win.on('close', () => { win = null });
+  win.webContents.on('dom-ready', () => {
+    if (win) {
+      win.show();
+    }
+  });
+}
 
 export function setMenu(mainWindow: Electron.BrowserWindow) {
   const template = [
     {
       label: 'File',
       submenu: [
+        {
+          label: 'Init repo',
+          accelerator: 'CmdOrCtrl+I',
+          click: () => createModalWindow(mainWindow, `file://${__dirname}/../renderer/init-repo.html`)
+        },
         {
           label: 'Open repo',
           accelerator: 'CmdOrCtrl+O',
@@ -28,5 +52,5 @@ export function setMenu(mainWindow: Electron.BrowserWindow) {
   ];
 
   const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+  mainWindow.setMenu(menu);
 }

2325e1b3ae809e2f87d930022e1c33472fb71e61
Pierre Vigier <pierre.vigier@ymail.com>
2019-01-08 22:57:52 +0100

    Add checks on the inputs to enable buttons and simplify code

--- src/renderer/clone-repo.ts
+++ src/renderer/clone-repo.ts
@@ -4,65 +4,71 @@ import { remote } from 'electron';
 // Folder input
 
 const folderInput = document.getElementById('folder') as HTMLInputElement;
+folderInput.addEventListener('input', () => {
+  updateButton();
+});
 
 // URL input
 
 const urlInput = document.getElementById('url') as HTMLInputElement;
-if (urlInput) {
-  urlInput.addEventListener('input', () => {
-    if (folderInput) {
-      folderInput.setAttribute('placeholder', Path.parse(urlInput.value).name);
-    }
-  });
-}
+urlInput.addEventListener('input', () => {
+  folderInput.placeholder = Path.parse(urlInput.value).name;
+  updateButton();
+});
 
 // Prefix paragraph
 
-const prefixParagraph = document.getElementById('prefix');
+const prefixParagraph = document.getElementById('prefix') as HTMLParagraphElement;
 function updatePrefixParagraph() {
-  if (prefixParagraph && pathInput) {
-    prefixParagraph.textContent = pathInput.value;
-  }
+  prefixParagraph.textContent = pathInput.value;
 }
 
 // Path input
 
 const pathInput = document.getElementById('path') as HTMLInputElement;
-if (pathInput) {
-  pathInput.addEventListener('input', () => {
-    updatePrefixParagraph();
-  });
-}
+pathInput.addEventListener('input', () => {
+  updatePrefixParagraph();
+  updateButton();
+});
 
 // Browse button
 
-const browseButton = document.getElementById('browse');
-if (browseButton) {
-  browseButton.addEventListener('click', () => {
-    const window = remote.BrowserWindow.getFocusedWindow()!;
-    remote.dialog.showOpenDialog(window, {properties: ['openDirectory']}, (paths) => {
-      if (paths && pathInput) {
-        pathInput.setAttribute('value', paths[0]);
-        updatePrefixParagraph();
-      }
-    });
+const browseButton = document.getElementById('browse') as HTMLButtonElement;
+browseButton.addEventListener('click', () => {
+  const window = remote.BrowserWindow.getFocusedWindow()!;
+  remote.dialog.showOpenDialog(window, {properties: ['openDirectory']}, (paths) => {
+    if (paths) {
+      pathInput.value = paths[0];
+      updatePrefixParagraph();
+      updateButton();
+    }
   });
-}
+});
 
 // Clone repository button
 
-const cloneButton = document.getElementById('clone');
-if (cloneButton) {
-  cloneButton.addEventListener('click', () => {
-    if (pathInput && urlInput) {
-      const window = remote.BrowserWindow.getFocusedWindow()!;
-      const pathPrefix = pathInput.value;
-      const folderName = folderInput.value || folderInput.getAttribute('placeholder');
-      if (pathPrefix && folderName) {
-        const path = Path.join(pathPrefix, folderName);
-        window.getParentWindow().webContents.send('clone-repo', [path, urlInput.value]);
-        window.close();
-      }
-    }
-  });
+function getData() {
+  const pathPrefix = pathInput.value;
+  const folderName = folderInput.value || folderInput.getAttribute('placeholder');
+  const url = urlInput.value;
+  return [pathPrefix, folderName, url];
 }
+
+const cloneButton = document.getElementById('clone') as HTMLButtonElement;
+function updateButton() {
+  let disabled = true;
+  const [pathPrefix, folderName, url] = getData();
+  if (pathPrefix && folderName && url) {
+    disabled = false;
+  }
+  cloneButton.disabled = disabled;
+}
+cloneButton.addEventListener('click', () => {
+  const window = remote.BrowserWindow.getFocusedWindow()!;
+  const [pathPrefix, folderName, url] = getData();
+  if (pathPrefix && folderName && url) {
+    const path = Path.join(pathPrefix, folderName);
+    window.getParentWindow().webContents.send('clone-repo', [path, url]);
+    window.close();
+  }
+});
--- src/renderer/init-repo.ts
+++ src/renderer/init-repo.ts
@@ -4,65 +4,70 @@ import { remote } from 'electron';
 // Folder input
 
 const folderInput = document.getElementById('folder') as HTMLInputElement;
+folderInput.addEventListener('input', () => {
+  updateButton();
+});
 
 // Name input
 
 const nameInput = document.getElementById('name') as HTMLInputElement;
-if (nameInput) {
-  nameInput.addEventListener('input', () => {
-    if (folderInput) {
-      folderInput.setAttribute('placeholder', nameInput.value);
-    }
-  });
-}
+nameInput.addEventListener('input', () => {
+  folderInput.placeholder = nameInput.value;
+  updateButton();
+});
 
 // Prefix paragraph
 
-const prefixParagraph = document.getElementById('prefix');
+const prefixParagraph = document.getElementById('prefix') as HTMLParagraphElement;
 function updatePrefixParagraph() {
-  if (prefixParagraph && pathInput) {
-    prefixParagraph.textContent = pathInput.value;
-  }
+  prefixParagraph.textContent = pathInput.value;
 }
 
 // Path input
 
 const pathInput = document.getElementById('path') as HTMLInputElement;
-if (pathInput) {
-  pathInput.addEventListener('input', () => {
-    updatePrefixParagraph();
-  });
-}
+pathInput.addEventListener('input', () => {
+  updatePrefixParagraph();
+  updateButton();
+});
 
 // Browse button
 
-const browseButton = document.getElementById('browse');
-if (browseButton) {
-  browseButton.addEventListener('click', () => {
-    const window = remote.BrowserWindow.getFocusedWindow()!;
-    remote.dialog.showOpenDialog(window, {properties: ['openDirectory']}, (paths) => {
-      if (paths && pathInput) {
-        pathInput.setAttribute('value', paths[0]);
-        updatePrefixParagraph();
-      }
-    });
+const browseButton = document.getElementById('browse') as HTMLButtonElement;
+browseButton.addEventListener('click', () => {
+  const window = remote.BrowserWindow.getFocusedWindow()!;
+  remote.dialog.showOpenDialog(window, {properties: ['openDirectory']}, (paths) => {
+    if (paths) {
+      pathInput.value = paths[0];
+      updatePrefixParagraph();
+      updateButton();
+    }
   });
-}
+});
 
 // Create repository button
 
-const createButton = document.getElementById('create');
-if (createButton) {
-  createButton.addEventListener('click', () => {
-    if (pathInput && folderInput) {
-      const window = remote.BrowserWindow.getFocusedWindow()!;
-      const pathPrefix = pathInput.value;
-      const folderName = folderInput.value || folderInput.getAttribute('placeholder');
-      if (pathPrefix && folderName) {
-        const path = Path.join(pathPrefix, folderName);
-        window.getParentWindow().webContents.send('init-repo', path);
-        window.close();
-      }
-    }
-  });
+function getData() {
+  const pathPrefix = pathInput.value;
+  const folderName = folderInput.value || folderInput.getAttribute('placeholder');
+  return [pathPrefix, folderName];
 }
+
+const createButton = document.getElementById('create') as HTMLButtonElement;
+function updateButton() {
+  let disabled = true;
+  const [pathPrefix, folderName] = getData();
+  if (pathPrefix && folderName) {
+    disabled = false;
+  }
+  createButton.disabled = disabled;
+}
+createButton.addEventListener('click', () => {
+  const window = remote.BrowserWindow.getFocusedWindow()!;
+  const [pathPrefix, folderName] = getData();
+  if (pathPrefix && folderName) {
+    const path = Path.join(pathPrefix, folderName);
+    window.getParentWindow().webContents.send('init-repo', path);
+    window.close();
+  }
+});

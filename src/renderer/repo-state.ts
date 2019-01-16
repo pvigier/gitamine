95be383141715c343e70426e8c0c9ed5828115e8
Pierre Vigier <pierre.vigier@ymail.com>
2019-01-16 23:36:13 +0100

    Add incremental updates in RepoState

--- src/renderer/repo-state.ts
+++ src/renderer/repo-state.ts
@@ -30,19 +30,21 @@ export class RepoState {
     this.parents = new Map<string, string[]>();
     this.children = new Map<string, [string, ChildrenType][]>();
 
-    this.load(path, onReady);
+    this.update(path, onReady);
   }
 
-  async load(path: string, onReady: () => void) {
+  async update(path: string, onReady: () => void) {
     try {
       this.repo = await Git.Repository.open(path);
       const names = await this.repo.getReferenceNames(Git.Reference.TYPE.OID);
-      await this.getReferenceCommits(names);
-      await this.getAllCommits();
+      const referencesToUpdate = await this.getReferenceCommits(names);
+      console.log(referencesToUpdate);
+      const newCommits = await this.getNewCommits(referencesToUpdate);
+      console.log(newCommits);
+      await this.getParents(newCommits);
+      this.removeUnreachableCommits();
+      this.sortCommits();
       await this.getHead();
-      await this.getParents();
-      this.updateChildren();
-      this.topologicalSort();
       this.graph = new CommitGraph(this);
       onReady();
     } catch (e) {
@@ -50,76 +52,143 @@ export class RepoState {
     }
   }
 
-  getReferenceCommits(names: string[]) {
-    return Promise.all(names.map(async (name) => {
+  async getReferenceCommits(names: string[]) {
+    // Get reference commits
+    const referenceCommits = await Promise.all(names.map(async (name) => {
       try {
-        const commit = await this.repo.getReferenceCommit(name);
-        this.references.set(name, commit);
+        return [name, await this.repo.getReferenceCommit(name)] as [string, Git.Commit];
+      } catch (e) {
+        console.error(e);
+        return [name, null] as [string, null];
+      }
+    }));
+    // Update references and shaToReferences
+    const referencesToUpdate: string[] = [];
+    const newReferences = new Map<string, Git.Commit>();
+    this.shaToReferences.clear();
+    for (let [name, commit] of referenceCommits) {
+      if (commit) {
+        if (this.references.get(name) !== commit) {
+          referencesToUpdate.push(name);
+        }
+        newReferences.set(name, commit);
         if (!this.shaToReferences.has(commit.sha())) {
           this.shaToReferences.set(commit.sha(), []);
         }
         this.shaToReferences.get(commit.sha())!.push(name);
-      } catch (e) {
-        console.error(e);
       }
-    }))
+    }
+    this.references = newReferences;
+    return referencesToUpdate;
   }
 
-  async getAllCommits() {
+  async getNewCommits(references: string[]) {
     const walker = Git.Revwalk.create(this.repo);
-    walker.sorting(Git.Revwalk.SORT.TIME);
-    for (let name of this.references.keys()) {
+    for (let name of references) {
       walker.pushRef(name); 
     }
-    this.commits = await walker.getCommitsUntil(() => true);
-    for (let commit of this.commits) {
-      this.shaToCommit.set(commit.sha(), commit);
+    // TODO: find a way to stop earlier the exploration
+    const commits = await walker.getCommitsUntil(() => true);
+    const newCommits: Git.Commit[] = [];
+    for (let commit of commits) {
+      const sha = commit.sha();
+      if (!this.shaToCommit.has(sha)) {
+        this.commits.push(commit);
+        this.shaToCommit.set(sha, commit);
+        newCommits.push(commit);
+      }
     }
+    return newCommits;
   }
 
-  async getHead() {
-    this.head = this.references.get((await this.repo.head()).name())!.sha();
-  }
-
-  getParents() {
-    return Promise.all(this.commits.map(async (commit) => {
-      const parents = await commit.getParents(Infinity);
-      this.parents.set(commit.sha(), parents.map(commit => commit.sha()));
+  getParents(commits: Git.Commit[]) {
+    for (let commit of commits) {
+      this.children.set(commit.sha(), []);
+    }
+    return Promise.all(commits.map(async (commit) => {
+      const commitSha = commit.sha();
+      const parentShas = (await commit.getParents(Infinity)).map(commit => commit.sha());
+      this.parents.set(commitSha, parentShas);
+      // Update children
+      for (let i = 0; i < parentShas.length; ++i) {
+        if (i === 0) {
+          this.children.get(parentShas[i])!.push([commitSha, ChildrenType.Commit]);
+        } else {
+          this.children.get(parentShas[i])!.push([commitSha, ChildrenType.Merge]);
+        }
+      }
     }));
   }
 
-  updateChildren() {
-    for (let commit of this.commits) {
-      this.children.set(commit.sha(), []);
+  removeUnreachableCommits() {
+    // Find unreach commits
+    const alreadyAdded = new Map<string, boolean>();
+    const frontier = [...this.references.values()];
+    for (let commit of frontier) {
+      alreadyAdded.set(commit.sha(), true);
     }
-    for (let [child, parents] of this.parents) {
-      for (let parent of parents) {
-        if (parents.length == 1) {
-          this.children.get(parent)!.push([child, ChildrenType.Commit]);
-        } else {
-          this.children.get(parent)!.push([child, ChildrenType.Merge]);
+    while (frontier.length !== 0) {
+      const commit = frontier.pop()!;
+      const commitSha = commit.sha();
+      for (let parentSha of this.parents.get(commitSha)!) {
+        if (!alreadyAdded.get(commitSha)) {
+          alreadyAdded.set(parentSha, true);
+          frontier.push(this.shaToCommit.get(parentSha)!);
         }
+      }
+    }
+
+    // Remove them
+    for (let [commitSha, seen] of alreadyAdded.entries()) {
+      if (!seen) {
+        this.removeCommit(this.shaToCommit.get(commitSha)!);
       }
     }
   }
 
-  topologicalSort() {
+  removeCommit(commit: Git.Commit) {
+    console.log('remove commit', commit.sha());
+    this.commits.splice(this.commits.indexOf(commit), 1);
+    const commitSha = commit.sha();
+    this.shaToCommit.delete(commitSha);
+    for (let parentSha of this.parents.get(commitSha)!) {
+      const parentChildren = this.children.get(parentSha)!;
+      parentChildren.splice(parentChildren.findIndex((x) => x[0] === commitSha), 1);
+    }
+    this.parents.delete(commitSha);
+    for (let [childSha, type] of this.children.get(commitSha)!) {
+      const childParents = this.parents.get(childSha)!;
+      childParents.splice(childParents.indexOf(commitSha), 1);
+    }
+    this.children.delete(commitSha);
+  }
+
+  sortCommits() {
+    // TODO: code the dfs with a stack
     const dfs = (commit: Git.Commit) => {
-      if (alreadySeen.get(commit.sha())) {
+      const commitSha = commit.sha();
+      if (alreadySeen.get(commitSha)) {
         return;
       }
-      alreadySeen.set(commit.sha(), true);
-      for (let [childSha, type] of this.children.get(commit.sha())!) {
+      alreadySeen.set(commitSha, true);
+      for (let [childSha, type] of this.children.get(commitSha)!) {
         dfs(this.shaToCommit.get(childSha)!);
       }
       sortedCommits.push(commit);
     }
 
+    // Sort the commits by date (from newer to older)
+    this.commits.sort((lhs, rhs) => rhs.date().valueOf() - lhs.date().valueOf());
+    // Topological sort (from parent to children)
     const sortedCommits: Git.Commit[] = [];
     const alreadySeen = new Map<string, boolean>();
     for (let commit of this.commits) {
       dfs(commit);
     }
     this.commits = sortedCommits;
+  }
+
+  async getHead() {
+    this.head = this.references.get((await this.repo.head()).name())!.sha();
   }
 }

59603e482ff905d44a41da7f0314f9a8c5956d11
Pierre Vigier <pierre.vigier@ymail.com>
2019-02-04 23:23:32 +0100

    Add buttons in the toolbar

--- src/renderer/components/toolbar.tsx
+++ src/renderer/components/toolbar.tsx
@@ -10,6 +10,13 @@ export class Toolbar extends React.PureComponent<ToolbarProps, {}> {
     return (
       <div className='repo-toolbar'>
         <h1>{this.props.repo.name}</h1>
+        <div className='toolbar-buttons'>
+          <button>Fetch</button>
+          <button>Push</button>
+          <button>Branch</button>
+          <button>Stash</button>
+          <button>Pop</button>
+        </div>
       </div>
     );
   }

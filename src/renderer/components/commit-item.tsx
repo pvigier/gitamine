93d2eeb57845a2d8d39f04bece3b1995ae889300
Pierre Vigier <pierre.vigier@ymail.com>
2019-02-04 23:27:23 +0100

    Add CommitItem's context menu

--- src/renderer/components/commit-item.tsx
+++ src/renderer/components/commit-item.tsx
@@ -1,3 +1,4 @@
+import { remote, clipboard } from 'electron';
 import * as React from 'react';
 import * as Git from 'nodegit';
 import { ReferenceBadge } from './reference-badge';
@@ -16,10 +17,34 @@ export class CommitItem extends React.PureComponent<CommitItemProps, {}> {
   constructor(props: CommitItemProps) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.handleContextMenu = this.handleContextMenu.bind(this);
   }
 
   handleClick(event: React.MouseEvent<HTMLLIElement>) {
     this.props.onCommitSelect(this.props.commit);
+  }
+
+  handleContextMenu(event: React.MouseEvent<HTMLLIElement>) {
+    const template = [
+      {
+        label: 'Create branch here',
+        click: () => console.log('branch')
+      },
+      {
+        label: 'Reset to this commit',
+        click: () => console.log('reset')
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Copy commit sha to clipboard',
+        click: () => clipboard.writeText(this.props.commit.sha())
+      },
+    ]
+    const menu = remote.Menu.buildFromTemplate(template);
+    event.preventDefault();
+    menu.popup({});
   }
 
   render() {
@@ -31,7 +56,9 @@ export class CommitItem extends React.PureComponent<CommitItemProps, {}> {
         key={name} />
     ));
     return (
-      <li className={this.props.selected ? 'selected-commit' : ''} onClick={this.handleClick}>
+      <li className={this.props.selected ? 'selected-commit' : ''} 
+        onClick={this.handleClick}
+        onContextMenu={this.handleContextMenu}>
         {badges}{this.props.commit.message()}
       </li>
     );

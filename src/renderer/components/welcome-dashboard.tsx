038ad31f875065aa0247272b3cf8e83753b93042
Pierre Vigier <pierre.vigier@ymail.com>
2019-02-18 12:44:43 +0100

    Improve the displaying of paths in the welcome dashboard

--- src/renderer/components/welcome-dashboard.tsx
+++ src/renderer/components/welcome-dashboard.tsx
@@ -1,6 +1,16 @@
+import * as os from 'os';
+import * as Path from 'path';
 import * as React from 'react';
 import { Field, Settings } from '../../shared/settings';
 import { getRepoName } from '../helpers/repo-state';
+
+function getWorkingDirectory(path: string) {
+  if (path.endsWith('.git')) {
+    return Path.dirname(path);
+  } else {
+    return path;
+  }
+}
 
 export class WelcomeDashboardProps {
   onRecentlyOpenedRepoClick: (path: string) => void;
@@ -8,17 +18,31 @@ export class WelcomeDashboardProps {
   onInitRepo: () => void;
   onCloneRepo: () => void;
 }
+
 export class WelcomeDashboard extends React.PureComponent<WelcomeDashboardProps, {}> {
   render() {
+    const homeDir = os.homedir();
     const recentlyOpenedItems = Settings.get(Field.RecentlyOpened, [])
-      .map((path: string) => (
-        <div className='action' 
-          onClick={() => this.props.onRecentlyOpenedRepoClick(path)}
-          key={path}>
-          <h3>{getRepoName(path)}</h3>
-          <p>{path}</p>
-        </div>
-      ));
+      .map((path: string) => {
+        const relativePath = Path.relative(homeDir, path);
+        const formattedPath = relativePath.startsWith('..') ? 
+          Path.normalize(getWorkingDirectory(path)) : 
+          `~${Path.sep}${getWorkingDirectory(relativePath)}`;
+        const i = formattedPath.lastIndexOf(Path.sep);
+        return (
+          <div className='action' 
+            onClick={() => this.props.onRecentlyOpenedRepoClick(path)}
+            key={path}>
+            <h3>{getRepoName(path)}</h3>
+            <p> 
+              <span className='ellipsis-middle'>
+                <span className='left'>{formattedPath.substr(0, i)}</span>
+                <span className='right'>{formattedPath.substr(i)}</span>
+              </span>
+            </p>
+          </div>
+        );
+      });
     return (
       <div className='welcome-dashboard'>
         <h1>Welcome to gitamine!</h1>
